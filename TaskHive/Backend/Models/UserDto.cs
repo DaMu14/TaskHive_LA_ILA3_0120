8157a8344cd38d653aeda004b0523fe0f1dc0242
DaMu14 <da407mu@gmail.com>
2024-11-29 10:23:39 +0100

    Login länge #13

--- TaskHive/Backend/Models/UserDto.cs
+++ TaskHive/Backend/Models/UserDto.cs
@@ -1,8 +1,14 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
 {
     public class UserDto
     {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Das Passwort muss mindestens 5 Zeichen lang sein.")]
+        public string Password { get; set; } = string.Empty;
     }
 }
